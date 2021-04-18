101946053e9c48482af5b8f803e8cf5264930145
student <shreya.priya@hcl.com>
2021-04-18 10:27:22 +0000

    initial commit

---
+++ src/main/java/com/shreya/springboot/devops/graded/gradedassignment/GradedAssignmentApplication.java
@@ -0,0 +1,14 @@
+package com.shreya.springboot.devops.graded.gradedassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradedAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradedAssignmentApplication.class, args);
+		System.out.println("Hello Dev-Ops");
+	}
+
+}
---
+++ src/test/java/com/shreya/springboot/devops/graded/gradedassignment/GradedAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shreya.springboot.devops.graded.gradedassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradedAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
